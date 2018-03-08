b0b3987d3814c4fe57e11ad360951201694530bf
Jonathan Norstrom <jon.norstrom@gmail.com>
2018-03-08 11:14:14 -0500

    add testing tests to test your testing test abilities

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,38 @@
 class User < ApplicationRecord
+  validate :check_password_length
+  validate :req_chars_for_password
   validates :username, uniqueness: true
   validates :username, length: { minimum: 6 }
   has_secure_password
+
+
+  def check_password_length
+    if password && password.length < 6
+      errors.add(:password, "can't be less than 6 characters")
+    end
+  end
+
+  def req_chars_for_password
+    errors.add(:password, "must contain certain stuff") unless (has_lowercase && has_uppercase && has_digit)
+  end
+
+  private
+
+  def has_lowercase
+    # fancy ruby stuff to analyze password
+    # true/false
+    true
+  end
+
+  def has_uppercase
+    ## same thing
+    # true/false
+    true
+  end
+
+  def has_digit
+    ## same thing
+    # true/false
+    true
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,30 @@
 require "rails_helper"
 
 RSpec.describe User, :type => :model do
-  context "making a new user" do
+  context "when making a new user:" do
     it "doesn't make a user without a username" do
       user = User.new(password: "password")
       user.save
       expect(user.id).to be_nil
+    end
+
+    it "secures our password as it saves" do
+      user = User.new(username: "jonjon", password: "password")
+      user.save
+      expect(user.password_digest).not_to eq('password')
+    end
+
+    it "validates correct password characters" do
+      user = User.new(username: "jonjon", password: "password")
+      other_user = User.new(username: "jonjon", password: "password1")
+      another_user = User.new(username: "jonjon", password: "Password1")
+      user.save
+      other_user.save
+      another_user.save
+
+      expect(user.id).to be_nil
+      expect(other_user.id).to be_nil
+      expect(other_user.id).not_to be_nil
     end
   end
 end
